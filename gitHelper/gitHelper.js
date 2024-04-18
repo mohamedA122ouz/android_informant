a49669fbba68d2a1778af831d364dcc856d02e1c
unknown <studyatmohamed@gmail.com>
2024-04-19 00:18:41 +0200

    continuing to develop

--- gitHelper/gitHelper.js
+++ gitHelper/gitHelper.js
@@ -1,8 +1,24 @@
 import read from "prompt-sync";
 import history from "prompt-sync-history";
-import fs from "fs/promises";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const prompt = read({
     history:history()
 });
-fs.
-let username = prompt("Enter your name: ");
+function createSettingsFile(){
+    const settings = {
+        username : prompt("Enter your name: "),
+        gitRepo : prompt("Add cloning Repo: ")
+        
+    }
+
+}
+let gitSettings = null;
+try{
+    gitSettings = fs.readFileSync(path.join(__filename,"./gitSettings.json"));
+}catch{
+    
+}
