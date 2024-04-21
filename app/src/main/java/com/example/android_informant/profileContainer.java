188c9130e7a4856d28045a90908f8f32d88254bc
mohameda122ouz <studyatmohamed@gmail.com>
2024-04-21 20:17:09 +0200

    profile container

--- app/src/main/java/com/example/android_informant/profileContainer.java
+++ app/src/main/java/com/example/android_informant/profileContainer.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Camera extends AppCompatActivity {
+public class profileContainer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        setContentView(R.layout.activity_profilecontainer);
     }
 }
